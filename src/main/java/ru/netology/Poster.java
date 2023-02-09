a2b380d665f6ac3b181e5f8491d12e25cb475fdb
Grisha Petrosyan <petrosyan.grigorij2018@yandex.ru>
2023-02-09 10:22:48 +0300

    Gitignoredddd

--- src/main/java/ru/netology/Poster.java
+++ src/main/java/ru/netology/Poster.java
@@ -1,10 +1,12 @@
 package ru.netology;
+
 public class Poster {
     private String Id;
     private String Title;
     private int ReleaseYear;
     private String ImageUrl;
-    public Poster(String Id,String Title, int ReleaseYear, String ImageUrl){
+
+    public Poster(String Id, String Title, int ReleaseYear, String ImageUrl) {
         this.Id = Id;
         this.Title = Title;
         this.ReleaseYear = ReleaseYear;
--- src/main/java/ru/netology/PosterManager.java
+++ src/main/java/ru/netology/PosterManager.java
@@ -1,34 +1,40 @@
 package ru.netology;
+
 public class PosterManager {
-    private Poster[] posters = new  Poster[0];
+    private Poster[] posters = new Poster[0];
     private int MaxPosters = 10;
-    public PosterManager(int MaxPosters){
+
+    public PosterManager(int MaxPosters) {
         this.MaxPosters = MaxPosters;
     }
-    public PosterManager(){
+
+    public PosterManager() {
 
     }
-    public void Save (Poster poster){
+
+    public void Save(Poster poster) {
         Poster[] tmp = new Poster[posters.length + 1];
-        for (int i = 0; i < posters.length; i++){
-            tmp[i]= posters[i];
+        for (int i = 0; i < posters.length; i++) {
+            tmp[i] = posters[i];
         }
-        tmp[tmp.length- 1]=poster;
-        posters= tmp;
+        tmp[tmp.length - 1] = poster;
+        posters = tmp;
     }
-    public Poster[]findAll(){
+
+    public Poster[] findAll() {
         return posters;
     }
-    public Poster[]findLast(){
+
+    public Poster[] findLast() {
         int ResultLenght;
-        if (MaxPosters>=10){
+        if (MaxPosters >= 10) {
             ResultLenght = 10;
         } else {
-            ResultLenght= MaxPosters;
+            ResultLenght = MaxPosters;
         }
-        Poster[]reversed = new Poster[ResultLenght];
-        for ( int i = 0; i < reversed.length; i ++){
-            reversed[i]=posters[posters.length - 1 - i];
+        Poster[] reversed = new Poster[ResultLenght];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = posters[posters.length - 1 - i];
         }
         return reversed;
     }
